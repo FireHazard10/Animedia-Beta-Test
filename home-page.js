b9159963dc6111a045e0819dc2231b9ca407b9ad
FireHazard10 <francovisal@gmail.com>
2026-04-26 04:29:19 +0000

    Add files via upload

---
+++ home-page.js
@@ -0,0 +1,25 @@
+//hamburger menu
+document.querySelectorAll(".badge").forEach((b) => {
+  b.addEventListener("click", () => b.classList.toggle("actixe"));
+});
+
+function toggleMobileMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.classList.toggle("active");
+}
+
+//clock Timer
+function updateClock() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString();
+  document.getElementById("clock").textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+function scrollWeek(distance) {
+  document
+    .getElementById("weekDays")
+    .scrollBy({ left: distance, behavior: "smooth" });
+}
